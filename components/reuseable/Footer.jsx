7a9f62fcb5588c90b17b8d259252e5bfd6d6b844
nzejoe <jonathannzete@gmail.com>
2024-04-28 08:58:09 +0100

    footer => copyright section

--- components/reuseable/Footer.jsx
+++ components/reuseable/Footer.jsx
@@ -52,54 +52,71 @@ const Footer = () => {
         },
     ];
     return (
-        <div className="bg-secondary/10 pt-14 pb-20 border-b">
-            <section className="px-5 md:grid md:grid-cols-2 gap-10 xl:grid-cols-4">
-                <div className={"mb-7 md:mb-0"}>
-                    <h3 className="text-lg uppercase font-semibold mb-3">contact us</h3>
-                    <div className={"text-black"}>
-                        <p>No 56 CDA Road, Via Ita-Oluwo, Ikorodu.</p>
-                        <p>Lagos, Nigeria</p>
-                        <p>Phone: +2348064547029</p>
-                        <p>Email: jonathannzete@gmail.com</p>
-                    </div>
-                </div>
-                {items.map((item) => (
-                    <div key={item.id} className={"mb-7 md:mb-0"}>
-                        <h3 className="text-lg uppercase font-semibold mb-3">{item.label}</h3>
-                        <div className="">
-                            {item.subItems.map((sub) => (
-                                <button
-                                    key={sub.id}
-                                    className="block transition-all duration-500 hover:underline hover:ml-2"
-                                >
-                                    {sub.label}
-                                </button>
-                            ))}
+        <section>
+            <div className="bg-secondary/10 pt-14 pb-20">
+                <div className="px-5 pb-5 border-b-[0.5px] border-primary md:grid md:grid-cols-2 gap-10 xl:grid-cols-4">
+                    <div className={"mb-7 md:mb-0"}>
+                        <h3 className="text-lg uppercase font-semibold mb-3">contact us</h3>
+                        <div className={"text-black"}>
+                            <p>No 56 CDA Road, Via Ita-Oluwo, Ikorodu.</p>
+                            <p>Lagos, Nigeria</p>
+                            <p>Phone: +2348064547029</p>
+                            <p>Email: jonathannzete@gmail.com</p>
                         </div>
                     </div>
-                ))}
-                <div className={""}>
-                    <h3 className="text-lg uppercase font-semibold mb-3">follow us</h3>
-                    <div className={"flex items-center space-x-4 text-lg"}>
-                        <button className="transition-transform duration-300 hover:scale-125">
-                            <BsFacebook className={""} />
-                        </button>
-                        <button className="transition-transform duration-300 hover:scale-125">
-                            <BsTwitter className={""} />
-                        </button>
-                        <button className="transition-transform duration-300 hover:scale-125">
-                            <BsPinterest className={""} />
-                        </button>
-                        <button className="transition-transform duration-300 hover:scale-125">
-                            <BsInstagram className={""} />
-                        </button>
-                        <button className="transition-transform duration-300 hover:scale-125">
-                            <BsYoutube className={""} />
-                        </button>
+                    {items.map((item) => (
+                        <div key={item.id} className={"mb-7 md:mb-0"}>
+                            <h3 className="text-lg uppercase font-semibold mb-3">{item.label}</h3>
+                            <div className="">
+                                {item.subItems.map((sub) => (
+                                    <button
+                                        key={sub.id}
+                                        className="block transition-all duration-500 hover:underline hover:ml-2"
+                                    >
+                                        {sub.label}
+                                    </button>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                    <div className={""}>
+                        <h3 className="text-lg uppercase font-semibold mb-3">follow us</h3>
+                        <div className={"flex items-center space-x-4 text-lg"}>
+                            <button className="transition-transform duration-300 hover:scale-125">
+                                <BsFacebook className={""} />
+                            </button>
+                            <button className="transition-transform duration-300 hover:scale-125">
+                                <BsTwitter className={""} />
+                            </button>
+                            <button className="transition-transform duration-300 hover:scale-125">
+                                <BsPinterest className={""} />
+                            </button>
+                            <button className="transition-transform duration-300 hover:scale-125">
+                                <BsInstagram className={""} />
+                            </button>
+                            <button className="transition-transform duration-300 hover:scale-125">
+                                <BsYoutube className={""} />
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </section>
-        </div>
+
+                <div className={"px-5 py-10 text-black/50"}>
+                    <div className="copyright text-sm text-center mb-5 md:text-base">
+                        <p className="">
+                            &copy; {new Date().getFullYear()} <span>EcoMarket</span> powered by{" "}
+                            <span className={"inline-flex items-center font-bold"}>
+                                codeGenesis <BsCodeSlash className={"text-3xl"} />
+                            </span>
+                        </p>
+                    </div>
+                    <div className={"flex justify-center h-8 xl:justify-end"}>
+                        <img src="/images/misc/payment-paypal.png" alt="" className="h-full object-contain" />
+                        <img src="/images/misc/payments.png" alt="" className="h-full object-contain" />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
