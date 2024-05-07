2af3e01cbbd7127506071f13ca17ad7d3c065cf2
nzejoe <jonathannzete@gmail.com>
2024-05-07 15:31:07 +0100

    swiperjs  installed

--- components/store/TopCategories.jsx
+++ components/store/TopCategories.jsx
@@ -2,6 +2,10 @@ import React, { Fragment } from "react";
 import Link from "next/link";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+// swipperjs
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Navigation } from "swiper";
 import LoadingSkeleton from "../reuseable/LoadingSkeleton";
 
 const TopCategories = () => {
@@ -19,37 +23,53 @@ const TopCategories = () => {
 
     return (
         <section className="section">
-            <h1 className="section-title">Top categories</h1>
-            <div className={"md:grid grid-cols-2 gap-5"}>
-                {topCategories.map((category, idx) => (
-                    <Fragment key={category.id}>
-                        {!isSuccess ? (
-                            <div className="h-[12.2rem] rounded-x overflow-hidden mb-5 md:mb-0 md:h-[16rem] xl:h-[20rem]">
-                                <LoadingSkeleton />
-                            </div>
-                        ) : (
-                            <div
-                                className={
-                                    "h-[13rem] md:h-[16rem] xl:h-[20rem] relative border border-primary rounded-xv overflow-hidden mb-5 md:mb-0"
-                                }
-                            >
-                                <div className={"w-full h-full"}>
-                                    <img
-                                        src={category.image}
-                                        alt={category.product_name}
-                                        className="w-full h-full object-cover"
-                                    ></img>
-                                    <div className={"absolute top-1/2 px-5"}>
-                                        <h2 className="font-semibold mb-2">{category.name}</h2>
-                                        <button className="button-regular button-outline border border-primary text-primary bg-white hover:bg-primary hover:text-white">
-                                            shop now
-                                        </button>
+            <h1 className="">Top categories</h1>
+            <div className={""}>
+                <Swiper
+                    slidesPerView={2}
+                    spaceBetween={10}
+                    navigation={true}
+                    modules={[Navigation]}
+                    breakpoints={{
+                        768: {
+                            slidesPerView: 2,
+                            centeredSlides: false,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                        },
+                    }}
+                >
+                    {topCategories.map((category, idx) => (
+                        <SwiperSlide key={category.id}>
+                            {!isSuccess ? (
+                                <div className="h-[12.2rem] rounded-x overflow-hidden mb-5 md:mb-0 md:h-[16rem] xl:h-[20rem]">
+                                    <LoadingSkeleton />
+                                </div>
+                            ) : (
+                                <div
+                                    className={
+                                        "h-[13rem] md:h-[16rem] xl:h-[20rem] relative border border-primary rounded-xv overflow-hidden mb-5 md:mb-0"
+                                    }
+                                >
+                                    <div className={"w-full h-full"}>
+                                        <img
+                                            src={category.image}
+                                            alt={category.product_name}
+                                            className="w-full h-full object-cover"
+                                        ></img>
+                                        <div className={"absolute top-1/2 px-5"}>
+                                            <h2 className="font-semibold mb-2">{category.name}</h2>
+                                            <button className="button-regular button-outline border border-primary text-primary bg-white hover:bg-primary hover:text-white">
+                                                shop now
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )}
-                    </Fragment>
-                ))}
+                            )}
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </section>
     );
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainLayout from "@/components/reuseable/MainLayout";
 import "@/styles/globals.css";
 
+// swiperjs
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 export default function App({ Component, pageProps }) {
     // Create a client
     const queryClient = new QueryClient();
